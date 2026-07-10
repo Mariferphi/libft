823d1349c1657470aa1b1367dfc36712715a7033
Mariphi <mafernandabecerraolivos@gmail.com>
2026-07-10 17:15:52 +0200

    Add files via upload

--- git_libft/ft_split.c
+++ git_libft/ft_split.c
@@ -6,95 +6,92 @@
 /*   By: marbecer <marbecer@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 21:22:59 by marbecer          #+#    #+#             */
-/*   Updated: 2026/07/10 16:47:30 by marbecer         ###   ########.fr       */
+/*   Updated: 2026/07/10 17:13:24 by marbecer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_w(char const *s, char c)
+static int	count_words(char const *str, char c)
 {
 	int	i;
-	int	count;
+	int	words;
 
-	count = 0;
 	i = 0;
-	while (s[i])
+	words = 0;
+	while (str[i] != '\0')
 	{
-		while (s[i] == c)
+		while (str[i] == c)
 			i++;
-		if (s[i] && s[i] != c)
+		if (str[i] != '\0')
 		{
-			count++;
-			while (s[i] && s[i] != c)
+			words++;
+			while (str[i] && str[i] != c)
 				i++;
 		}
 	}
-	return (count);
+	return (words);
 }
 
-static char	*fill_word(char const *s, char c)
-{
-	int		j;
-	char	*str;
-
-	j = 0;
-	while (s[j] && s[j] != c)
-		j++;
-	str = malloc(j + 1);
-	if (!str)
-		return (NULL);
-	j = 0;
-	while (s[j] && s[j] != c)
-	{
-		str[j] = s[j];
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
-}
-
-char	**free_arr(char **arr)
+static int	len_word_array(char const *str, char c)
 {
 	int	i;
 
 	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
+	while (str[i] != '\0' && str[i] != c)
 		i++;
+	return (i);
+}
+
+static void	free_mem(char **s, int i)
+{
+	while (i >= 0)
+	{
+		free(s[i]);
+		i--;
 	}
-	free(arr);
-	return (NULL);
+	free(s);
+}
+
+static int	create_words(char **array, const char *str, char c)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] == c)
+			i++;
+		if (str[i] != '\0')
+		{
+			len = len_word_array(str + i, c);
+			array[j] = ft_substr(str, i, len);
+			if (array[j] == NULL)
+			{
+				free_mem(array, j - 1);
+				return (0);
+			}
+			i = i + len;
+			j++;
+		}
+	}
+	array[j] = NULL;
+	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**arr;
-	int		words;
-	int		i;
+	char	**array;
 
 	if (!s)
 		return (NULL);
-	words = count_w(s, c);
-	arr = malloc((words + 1) * sizeof(char *));
-	if (!arr)
+	array = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!array)
 		return (NULL);
-	i = 0;
-	while (i < words)
-	{
-		while (*s == c)
-			s++;
-		if (*s != c)
-		{
-			arr[i] = fill_word(s, c);
-			if (!arr[i])
-				return (free_arr(arr));
-			i++;
-			while (*s && *s != c)
-				s++;
-		}
-	}
-	arr[i] = NULL;
-	return (arr);
+	if (!create_words(array, s, c))
+		return (NULL);
+	return (array);
 }
